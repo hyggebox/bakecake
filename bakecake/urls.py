505db2e620022df5f27d1ce5bf95d1b7046ee57c
Anastasia <asya_apto@mail.ru>
2022-04-27 17:43:09 +0300

    Fix link names

--- bakecake/urls.py
+++ bakecake/urls.py
@@ -8,8 +8,8 @@ from .views import render_lk_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', render_index_page, name='index_page'),
-    path('lk', render_lk_page, name='lk_page'),
+    path('', render_index_page, name='render_index_page'),
+    path('lk', render_lk_page, name='render_lk_page'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('auth/', include('django.contrib.auth.urls')),
 ]
