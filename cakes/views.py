d619ab7fa5f23f4a7cb65d47e3b764fd65caab1e
Anastasia <asya_apto@mail.ru>
2022-04-27 16:58:09 +0300

    Add standard user registration/login/logout

--- cakes/views.py
+++ cakes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegistrationForm
+
+
+class SignUp(CreateView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy('render_lk_page')
+    template_name = 'registration/register.html'
