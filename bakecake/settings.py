830a979ffa5f8955698e835959d346476de41f2d
Anastasia <asya_apto@mail.ru>
2022-04-28 16:55:21 +0300

    Add header and styles to login and register pages

--- bakecake/settings.py
+++ bakecake/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cakes',
-    'rest_framework'
+    'rest_framework',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
