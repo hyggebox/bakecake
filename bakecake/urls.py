dd855b3c2719a5f348d786fcd7375f46186896af
clownkill <alehnovich90@yandex.ru>
2022-04-28 12:43:47 +0300

    add DRF / cake components handler

--- bakecake/urls.py
+++ bakecake/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import render_index_page
 from .views import render_lk_page
+from .views import cake_api
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', render_index_page, name='render_index_page'),
     path('lk', render_lk_page, name='render_lk_page'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/cake', cake_api),
 ]
--- bakecake/views.py
+++ bakecake/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
 
+from cakes.models import (CakeLevel, CakeShape, CakeTopping,
+                          CakeDecor, CakeBerry)
 
 def render_index_page(request):
     return render(request, 'index.html')
@@ -9,3 +14,54 @@ def render_lk_page(request):
     template = 'lk.html'
     # template = 'lk-order.html'
     return render(request, template)
+
+
+class CakeLevelSerializer(ModelSerializer):
+    class Meta:
+        model = CakeLevel
+        fields = ('id', 'level_count', 'price')
+
+
+class CakeShapeSerializer(ModelSerializer):
+    class Meta:
+        model = CakeShape
+        fields = ('id', 'shape', 'price')
+
+
+class CakeToppingSerializer(ModelSerializer):
+    class Meta:
+        model = CakeTopping
+        fields = ('id', 'cake_topping', 'price')
+
+
+class CakeDecorSerializer(ModelSerializer):
+    class Meta:
+        model = CakeDecor
+        fields = ('id', 'cake_decor', 'price')
+
+
+class CakeBerrySerializer(ModelSerializer):
+    class Meta:
+        model = CakeBerry
+        fields = ('id', 'cake_berry', 'price')
+
+
+@api_view(['GET', 'POST'])
+def cake_api(request):
+    if request.method == 'GET':
+        levels = CakeLevel.objects.all()
+        shapes = CakeShape.objects.all()
+        toppings = CakeTopping.objects.all()
+        decors = CakeDecor.objects.all()
+        berries = CakeBerry.objects.all()
+
+        components = {
+            'levels': CakeLevelSerializer(levels, many=True).data,
+            'shapes': CakeShapeSerializer(shapes, many=True).data,
+            'toppings': CakeToppingSerializer(toppings, many=True).data,
+            'decors': CakeDecorSerializer(decors, many=True).data,
+            'berries': CakeBerrySerializer(berries, many=True).data,
+
+        }
+
+        return Response(components)
