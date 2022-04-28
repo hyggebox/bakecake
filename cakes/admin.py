bcd3afe2d159cfcdcff29d416e23cc855daa306a
Anastasia <asya_apto@mail.ru>
2022-04-28 10:14:24 +0300

    Add CustomUser model with phonenumber as login

--- cakes/admin.py
+++ cakes/admin.py
@@ -5,6 +5,7 @@ from .models import Cake
 from .models import Customer
 from .models import Order
 from .models import (CakeDecor, CakeBerry, CakeShape, CakeTopping, CakeLevel)
+from .models import CustomUser
 
 
 @admin.register(Customer)
@@ -31,3 +32,10 @@ class DiscountAdmin(admin.ModelAdmin):
 @admin.register(CakeDecor, CakeBerry, CakeShape, CakeTopping, CakeLevel)
 class CakePartsAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(CustomUser)
+class UserAdminConfig(admin.ModelAdmin):
+    list_display = ('phonenumber', 'username', 'email', 'is_staff', 'is_admin', 'is_active')
+    search_fields = ('phonenumber', 'email', 'is_active')
+    list_filter = ('is_staff', 'is_admin', 'is_active')
